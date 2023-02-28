c014878e63d301eab41fa3d553cb8181b520d265
Vivek <vishwakarmavivek83@gmail.com>
2023-03-01 00:14:21 +0530

    Solution to Valid Anagram

---
+++ src/Arrays/ValidAnagram.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabets = new int[26];
+        for (char c :s.toCharArray()){
+            int index = c-'a';
+            alphabets[index]++;
+        }
+        for (char c: t.toCharArray()){
+            int index = c-'a';
+            alphabets[index]--;
+            if (alphabets[index]<0)
+                return false;
+        }
+        return Arrays.stream(alphabets).sum() == 0;
+    }
+}
